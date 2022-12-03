b4c0b074207c81c4408d574c943a4509f5cc4e4d
[Ixchel Lopez] <[ixchellopezalegre@gmail.com]>
2022-12-03 01:11:09 +0100

    Reseteando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
+import { VideojuegoComponent } from './componentes/videojuego/videojuego.component';
+import { VideojuegosComponent } from './componentes/videojuegos/videojuegos.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    ProfileComponent,
+    VideojuegoComponent,
+    VideojuegosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
